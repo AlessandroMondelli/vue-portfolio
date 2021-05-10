7cf4d23d4505747c970ab8f838cd06e93b2583d5
root <root@pop-os.localdomain>
2021-05-10 18:59:23 +0200

    Creazione Layout one page con main menu + home.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <jumbotron></jumbotron>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Jumbotron from '../components/layout/Jumbotron.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Jumbotron,
   }
 }
 </script>
+
